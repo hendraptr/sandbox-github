57b1f5ccd1aeddf2e27d09a8baf21adba6db3d8c
hendraptr <hendraptr@yahoo.com>
2020-04-30 21:01:41 +0800

    initial commit

---
+++ src/main/java/com/in28minutes/springboot/rest/example/SpringBoot2RestServiceApplication.java
@@ -0,0 +1,16 @@
+package com.in28minutes.springboot.rest.example;
+
+//import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@Slf4j
+@SpringBootApplication
+public class SpringBoot2RestServiceApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringBoot2RestServiceApplication.class, args);
+//		log.info("i want to keep this line!!!! application started!!!!");
+	}
+}
---
+++ src/main/java/com/in28minutes/springboot/rest/example/student/StudentResource.java
@@ -0,0 +1,87 @@
+package com.in28minutes.springboot.rest.example.student;
+
+import io.swagger.annotations.ApiOperation;
+//import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
+//@Slf4j
+@RestController
+public class StudentResource {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @GetMapping("/students")
+    public List<Student> retrieveAllStudents() {
+        return studentRepository.findAll();
+    }
+
+    @GetMapping("/students/{id}")
+    @ApiOperation(value = "Find student by id",
+            notes = "Also returns a link to retrieve all students with rel - all-students")
+    public Resource<Student> retrieveStudent(@PathVariable long id) {
+        Optional<Student> student = studentRepository.findById(id);
+
+//        log.info("Hello how are you!!!!!!");
+//        log.info("test");
+        if (!student.isPresent())
+            throw new StudentNotFoundException("id-" + id);
+        System.out.println("hello world and  hitachi ebworx!!!!!!!");
+        Resource<Student> resource = new Resource<Student>(student.get());
+
+        ControllerLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllStudents());
+
+        resource.add(linkTo.withRel("all-students"));
+
+        return resource;
+    }
+
+    @DeleteMapping("/students/{id}")
+    public void deleteStudent(@PathVariable long id) {
+        studentRepository.deleteById(id);
+    }
+
+    @PostMapping("/students")
+    public ResponseEntity<Object> createStudent(@RequestBody Student student) {
+        Student savedStudent = studentRepository.save(student);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(savedStudent.getId()).toUri();
+
+        return ResponseEntity.created(location).build();
+
+    }
+
+    @PutMapping("/students/{id}")
+    public ResponseEntity<Object> updateStudent(@RequestBody Student student, @PathVariable long id) {
+
+        Optional<Student> studentOptional = studentRepository.findById(id);
+
+        if (!studentOptional.isPresent())
+            return ResponseEntity.notFound().build();
+
+        student.setId(id);
+
+        studentRepository.save(student);
+
+        return ResponseEntity.noContent().build();
+    }
+}
